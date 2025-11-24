ab1e1380b9ef39fed99d3e13b7e14f90d087f747
ganbin <jlau.ice@gmail.com>
2025-11-24 12:22:05 +0800

    init

---
+++ src/main/java/com/jkr/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.jkr.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/get")
+    public String test() {
+        return "success";
+    }
+
+}
+
+
