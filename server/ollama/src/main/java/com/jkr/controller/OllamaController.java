a0e3b2fffcdc5dc1f99e9c3c2f790f3b5dfa0883
ganbin <jlau.ice@gmail.com>
2025-12-02 12:40:14 +0800

    feat(ollama): integrate ChatClient for Ollama interactions

--- server/ollama/src/main/java/com/jkr/controller/OllamaController.java
+++ server/ollama/src/main/java/com/jkr/controller/OllamaController.java
@@ -1,6 +1,7 @@
 package com.jkr.controller;
 
 import jakarta.annotation.Resource;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,8 +20,12 @@ public class OllamaController {
 
     //方式2
     @Resource
-    @Qualifier("ollamaChatModel")
     private ChatModel chatModel;
+
+    public final ChatClient chatClient;
+    public OllamaController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
 
     /**
      * <a href="http://127.0.0.1:8002/ollama/block">测试</a>
@@ -41,7 +46,24 @@ public class OllamaController {
      */
     @GetMapping("/stream")
     public Flux<String> stream(@RequestParam(name = "question", defaultValue = "你是谁") String question) {
+        // 阻塞方式
+        //return chatModel.call(question);
+        // 流式方式
         return chatModel.stream(question);
+    }
+
+    /**
+     * ChatClient 方式使用Ollama
+     * <a href="http://127.0.0.1:8002/ollama/client">测试</a>
+     * @param question 询问信息
+     * @return Flux<String>
+     */
+    @GetMapping("/client")
+    public Flux<String> client(@RequestParam(name = "question", defaultValue = "你是谁") String question) {
+        // 阻塞方式
+        //return chatClient.prompt().user(question).call().content();
+        // 流式方式
+        return chatClient.prompt().user(question).stream().content();
     }
 }
 
